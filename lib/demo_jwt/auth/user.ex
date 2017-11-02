ea4727d2686fe97527b8a43c9d8525319b88dbcb
zhulinpinyu <mulixiang199016@gmail.com>
2017-11-03 00:05:30 +0800

    修改changeset函数 使password 哈希化存储

--- lib/demo_jwt/auth/user.ex
+++ lib/demo_jwt/auth/user.ex
@@ -16,7 +16,26 @@ defmodule DemoJwt.Auth.User do
   @doc false
   def changeset(%User{} = user, attrs) do
     user
-    |> cast(attrs, [:username, :password_hash, :email])
-    |> validate_required([:username, :password_hash, :email])
+    |> cast(attrs, [:username, :email])
+    |> validate_required([:username, :email])
+    |> unique_constraint(:username)
+    |> unique_constraint(:email)
+  end
+
+  def registration_changeset(user, attrs) do
+    user
+    |> changeset(attrs)
+    |> cast(attrs, [:password])
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_password_hash()
+  end
+
+  def put_password_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: password}} ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(password))
+      _ ->
+        changeset
+    end
   end
 end
