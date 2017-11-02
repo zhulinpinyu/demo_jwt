43f97dab288cba26956cad562541e65bfad77e32
zhulinpinyu <mulixiang199016@gmail.com>
2017-11-02 23:28:11 +0800

    生成 User model 以及相应的View Controller

---
+++ lib/demo_jwt/auth/user.ex
@@ -0,0 +1,21 @@
+defmodule DemoJwt.Auth.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias DemoJwt.Auth.User
+
+
+  schema "users" do
+    field :email, :string
+    field :password_hash, :string
+    field :username, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(%User{} = user, attrs) do
+    user
+    |> cast(attrs, [:username, :password_hash, :email])
+    |> validate_required([:username, :password_hash, :email])
+  end
+end
